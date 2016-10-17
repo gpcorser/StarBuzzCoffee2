6e1a8861a927cc36c10ff09510f90787fad13c0f
gpcorser <gpcorser@svsu.edu>
2016-10-17 16:06:26 -0400

    revised

--- app/src/main/java/com/example/gpcorser/myapplication/DrinkActivity.java
+++ app/src/main/java/com/example/gpcorser/myapplication/DrinkActivity.java
@@ -1,9 +1,14 @@
 package com.example.gpcorser.myapplication;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -16,18 +21,59 @@ public class DrinkActivity extends AppCompatActivity {
 
         // get the drink from the intent
         int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+
+        // create a cursor
+        try {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query ("DRINK",
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    "_id = ?",
+                    new String[] {Integer.toString(drinkNo)},
+                    null, null, null);
+            // move to first record
+            if (cursor.moveToFirst()) {
+
+                // get drink details
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+
+                // populate drink name
+                TextView name = (TextView)findViewById(R.id.name);
+                name.setText(nameText);
+
+                // populate drink description
+                TextView description = (TextView)findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                // populate drink image
+                ImageView photo = (ImageView)findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+            }
+            cursor.close();
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        /*
         Drink drink = Drink.drinks[drinkNo];
 
         //populate drink image
         ImageView photo = (ImageView)findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResourceId());
-        photo.setContentDescription(drink.getName());
+        photo.setImageResource(drink.getImageResourceId()); // change
+        photo.setContentDescription(drink.getName()); // change
 
         TextView name = (TextView)findViewById(R.id.name);
-        name.setText(drink.getName());
+        name.setText(drink.getName()); // change
 
         TextView description = (TextView)findViewById(R.id.description);
-        description.setText(drink.getDescription());
+        description.setText(drink.getDescription()); // change
+
+        */
 
     }
 }
---
+++ app/src/main/java/com/example/gpcorser/myapplication/StarbuzzDatabaseHelper.java
@@ -0,0 +1,89 @@
+package com.example.gpcorser.myapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by gpcorser on 10/17/2016.
+ */
+
+public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz";
+    private static final int DB_VERSION = 3;
+
+    StarbuzzDatabaseHelper (Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        updateMyDatabase(db, 0, DB_VERSION);
+
+        /*
+        ContentValues espresso = new ContentValues();
+        espresso.put("NAME", "Espresso");
+        ContentValues americano = new ContentValues();
+        americano.put("NAME", "Americano");
+        ContentValues latte = new ContentValues();
+        latte.put("NAME", "Latte");
+        ContentValues mochachino = new ContentValues();
+        mochachino.put("NAME", "Mochachino");
+        ContentValues filter = new ContentValues();
+        filter.put("DESCRIPTION", "Filter");
+
+        db.execSQL("CREATE TABLE DRINK ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT, "
+                + "DESCRIPTION TEXT, "
+                + "IMAGE_RESOURCE_ID INTEGER);");
+
+        db.insert("DRINK", null, espresso);
+        db.insert("DRINK", null, americano);
+        db.insert("DRINK", null, latte);
+        db.insert("DRINK", null, mochachino);
+        db.insert("DRINK", null, filter);
+        */
+
+    }
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE DRINK ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "DESCRIPTION TEXT, "
+                    + "IMAGE_RESOURCE_ID INTEGER);");
+            insertDrink(db, "LatteD", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "CappuccinoD", "Espresso, hot milk and steamed milk foam", R.drawable.cappuccino);
+            insertDrink(db, "FilterD", "Our best drip coffee", R.drawable.filter);
+        }
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+        }
+        if (oldVersion < 3) {
+            insertDrink(db, "LatteD", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "CappuccinoD", "Espresso, hot milk and steamed milk foam", R.drawable.cappuccino);
+            insertDrink(db, "FilterD", "Our best drip coffee", R.drawable.filter);
+        }
+    }
+
+    private void insertDrink(SQLiteDatabase db, String name, String description, int resourceId) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+        db.insert("DRINK", null, drinkValues);
+    }
+
+}
